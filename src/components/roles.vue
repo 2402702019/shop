84f28b6ae4d8bec444b301e57505616747780f31
haoping <2402702019@qq.com>
2019-02-18 17:49:36 +0800

    取消权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -12,19 +12,24 @@
                         <!--  el-row>(el-col>el-tag+el-col>el-row>(el-col>el-tag+el-col>el-tag))-->
                         <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
                             <el-col :span="4">
-                                <el-tag closable type="danger">{{item1.authName}}</el-tag>
+                                <el-tag @close="deleRights(scope.row,item1)" closable type="danger">{{item1.authName}}</el-tag>
                                 <i class="el-icon-arrow-right"></i>
                             </el-col>
                             <el-col :span="20">
                                 <el-row class="level2" v-for="(item2,i) in item1.children" :key="item2.id">
                                     <el-col :span="4">
-                                        <el-tag closable type="info">{{item2.authName}}</el-tag>
+                                        <el-tag @close="deleRights(scope.row,item2)" closable type="info">{{item2.authName}}</el-tag>
                                         <i class="el-icon-arrow-right"></i>
                                     </el-col>
                                     <el-col :span="20">
-                                        <el-tag closable v-for="(item3,i) in item2.children" :key="item3.id" type="warning">{{item3.authName}}</el-tag>
+                                        <el-tag @close="deleRights(scope.row,item3)" closable v-for="(item3,i) in item2.children" :key="item3.id" type="warning">{{item3.authName}}</el-tag>
                                     </el-col>
                                 </el-row>
+                            </el-col>
+                        </el-row>
+                        <el-row v-if="scope.row.children.length===0">
+                            <el-col>
+                                <span>未分配权限</span>
                             </el-col>
                         </el-row>
                     </template>
@@ -57,6 +62,21 @@ export default {
     this.getRoles();
   },
   methods: {
+    //   取消权限
+    async deleRights(role, rights) {
+      const res = await this.$http.delete(
+        `roles/${role.id}/rights/${rights.id}`
+      );
+      // console.log(res)
+      const { meta: { msg, status }, data } = res.data;
+      if (status === 200) {
+        // 提示
+        this.$message.success(msg);
+        // 更新
+        // this.getRoles();
+        role.children = data
+      }
+    },
     showDiaSetRights() {},
     async getRoles() {
       const res = await this.$http.get(`roles`);
