9a9f0d5d94a89fa3d6b7b4ab50a852e0e1fc81cb
haoping <2402702019@qq.com>
2019-02-19 16:30:48 +0800

    添加商品-级联选择器

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -12,21 +12,69 @@
             <el-step title="商品内容"></el-step>
         </el-steps>
         <!-- 标签页tabs -->
-        <el-tabs v-model="active" tab-position="left">
-            <el-tab-pane label="基本信息" name="1">基本信息</el-tab-pane>
-            <el-tab-pane label="商品参数" name="2">商品参数</el-tab-pane>
-            <el-tab-pane label="商品属性" name="3">商品属性</el-tab-pane>
-            <el-tab-pane label="商品图片" name="4">商品图片</el-tab-pane>
-             <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-        </el-tabs>
+        <el-form class="form" label-position="top" label-width="80px" :model="form">
+            <el-tabs v-model="active" tab-position="left">
+                <el-tab-pane label="基本信息" name="1">
+                    <el-form-item label="商品名称">
+                        <el-input v-model="form.goods_name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="商品价格">
+                        <el-input v-model="form.goods_price"></el-input>
+                    </el-form-item>
+                    <el-form-item label="商品重量">
+                        <el-input v-model="form.goods_weight"></el-input>
+                    </el-form-item>
+                    <el-form-item label="商品数量">
+                        <el-input v-model="form.goods_number"></el-input>
+                    </el-form-item>
+                    <el-form-item label="商品分类">
+                        {{selectedOptions}}
+                        <el-cascader clearable expand-trigger="hover" :options="options" v-model="selectedOptions" @change="handleChange" :props="defaultProp"></el-cascader>
+                    </el-form-item>
+                </el-tab-pane>
+                <el-tab-pane label="商品参数" name="2">商品参数</el-tab-pane>
+                <el-tab-pane label="商品属性" name="3">商品属性</el-tab-pane>
+                <el-tab-pane label="商品图片" name="4">商品图片</el-tab-pane>
+                <el-tab-pane label="商品内容" name="5">商品内容</el-tab-pane>
+            </el-tabs>
+        </el-form>
     </el-card>
 </template>
 <script>
 export default {
   data() {
     return {
-      active: "1"
+      active: "1",
+
+      // 添加商品请求时的请求体
+      // goods_name	商品名称	不能为空
+      // goods_price	价格	不能为空
+      // goods_number	数量	不能为空
+      // goods_weight	重量	不能为空
+      // goods_introduce	介绍	可以为空
+      form: {
+        goods_name: "",
+        goods_cat: "",
+        goods_price: "",
+        goods_number: "",
+        goods_weight: "",
+        goods_introduce: "",
+        pics: [],
+        attrs: []
+      },
+      option:[],
+      selectedOptions:[],
+      defaultProp:{
+          label:"",
+          value:"",
+          children:""
+      }
     };
+  },
+  methods: {
+      handleChange(){
+
+      }
   }
 };
 </script>
@@ -36,6 +84,10 @@ export default {
 }
 .alert {
   margin-top: 20px;
+}
+.form {
+    height: 400px;
+    overflow:auto
 }
 </style>
 
