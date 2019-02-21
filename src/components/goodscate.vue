de401ca89e0b2311d1268722654bddfd98fae031
haoping <2402702019@qq.com>
2019-02-21 19:14:09 +0800

    商品分类-element-tree-grid配置

--- src/components/goodscate.vue
+++ src/components/goodscate.vue
@@ -18,14 +18,7 @@
         <!-- 级联选择器 (表单元素) -->
         <el-form-item label="分类" :label-width="formLabelWidth">
           {{selectedOptions}}
-          <el-cascader
-            expand-trigger="hover"
-            change-on-select
-            clearable
-            :options="caslist"
-            v-model="selectedOptions"
-            :props="defaultProp"
-          ></el-cascader>
+          <el-cascader expand-trigger="hover" change-on-select clearable :options="caslist" v-model="selectedOptions" :props="defaultProp"></el-cascader>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -44,15 +37,7 @@
       childKey->子级数据的key名
       expandAll->默认全展开
       -->
-      <el-tree-grid
-        prop="cat_name"
-        treeKey="cat_id"
-        parentKey="cat_pid"
-        levelKey="cat_level"
-        childKey="children"
-        label="商品分类"
-        width="140"
-      ></el-tree-grid>
+      <el-tree-grid prop="cat_name" treeKey="cat_id" parentKey="cat-pid" levelKey="cat_level" childKey="children" label="商品分类" width="140"></el-tree-grid>
 
       <el-table-column label="级别">
         <template slot-scope="scope">
@@ -78,28 +63,20 @@
     </el-table>
 
     <!-- 分页 -->
-    <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="pagenum"
-      :page-sizes="[5, 10, 15,20]"
-      :page-size="pagesize"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total"
-    ></el-pagination>
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagenum" :page-sizes="[5, 10, 15,20]" :page-size="pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
   </el-card>
 </template>
 
 <script>
 // var ElTreeGrid = require('element-tree-grid');
-import ElTreeGrid from 'element-tree-grid'
+import ElTreeGrid from "element-tree-grid";
 
 export default {
   components: {
     // el-tree-grid
     ElTreeGrid
   },
-  data () {
+  data() {
     return {
       list: [],
       pagenum: 1,
@@ -108,27 +85,27 @@ export default {
       dialogFormVisibleAdd: false,
       form: {
         cat_pid: -1,
-        cat_name: '',
+        cat_name: "",
         cat_level: -1
       },
-      formLabelWidth: '140px',
+      formLabelWidth: "140px",
       //  级联选择器要用的数据
       caslist: [],
       selectedOptions: [],
       defaultProp: {
-        value: 'cat_id',
-        label: 'cat_name',
-        children: 'children'
+        value: "cat_id",
+        label: "cat_name",
+        children: "children"
       }
-    }
+    };
   },
-  created () {
-    this.getGoodsCate()
+  created() {
+    this.getGoodsCate();
   },
 
   methods: {
     // 添加分类 - 发送请求
-    async addCate () {
+    async addCate() {
       // 发送post请求
 
       // cat_pid: -1, 父id
@@ -136,67 +113,65 @@ export default {
 
       // 如果没选择分类 -> 添加一级分类
       if (this.selectedOptions.length === 0) {
-        this.form.cat_pid = 0
-        this.form.cat_level = 0
+        this.form.cat_pid = 0;
+        this.form.cat_level = 0;
       }
 
       // 如果选了一个分类 -> 添加的是二级分类
       if (this.selectedOptions.length === 1) {
-        this.form.cat_pid = this.selectedOptions[0]
-        this.form.cat_level = 1
+        this.form.cat_pid = this.selectedOptions[0];
+        this.form.cat_level = 1;
       }
       // 如果选了两个分类,->添加三级分类
       if (this.selectedOptions.length === 2) {
-        this.form.cat_pid = this.selectedOptions[1]
-        this.form.cat_level = 2
+        this.form.cat_pid = this.selectedOptions[1];
+        this.form.cat_level = 2;
       }
 
-      const res = await this.$http.post(`categories`, this.form)
-      console.log(res)
-      const {
-        meta: { msg, status }
-      } = res.data
+      const res = await this.$http.post(`categories`, this.form);
+      console.log(res);
+      const { meta: { msg, status } } = res.data;
       if (status === 201) {
-        this.dialogFormVisibleAdd = false
-        this.getGoodsCate()
+        this.dialogFormVisibleAdd = false;
+        this.getGoodsCate();
       }
     },
 
     // 添加分类- 显示对话框
-    async addGoodsCate () {
+    async addGoodsCate() {
       // 获取两级分类的数据
-      const res = await this.$http.get(`categories?type=2`)
-      this.caslist = res.data.data
-      this.dialogFormVisibleAdd = true
+      const res = await this.$http.get(`categories?type=2`);
+      this.caslist = res.data.data;
+      this.dialogFormVisibleAdd = true;
     },
 
     // 获取所有分类
-    async getGoodsCate () {
+    async getGoodsCate() {
       const res = await this.$http.get(
         `categories?type=3&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
-      )
+      );
       // console.log(res)
-      this.list = res.data.data.result
-      console.log(this.list)
+      this.list = res.data.data.result;
+      console.log(this.list);
 
-      this.total = res.data.data.total
+      this.total = res.data.data.total;
     },
 
     // 分页的相关方法
-    handleSizeChange (val) {
+    handleSizeChange(val) {
       // console.log(`每页 ${val} 条`)
-      this.pagesize = val
-      this.pagenum = 1
-      this.getGoodsCate()
+      this.pagesize = val;
+      this.pagenum = 1;
+      this.getGoodsCate();
     },
-    handleCurrentChange (val) {
+    handleCurrentChange(val) {
       // console.log(`当前页: ${val}`)
       // this.pagenum = 2 = val
-      this.pagenum = val
-      this.getGoodsCate()
+      this.pagenum = val;
+      this.getGoodsCate();
     }
   }
-}
+};
 </script>
 
 <style>
